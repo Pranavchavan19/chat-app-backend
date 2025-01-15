fbe09e2f92a92d11c83091391ae78aea2ee97717
Pranavbc <pranav1952000@gmail.com>
2025-01-15 15:58:23 +0530

    time

--- src/main/java/com/substring/chat/entities/Message.java
+++ src/main/java/com/substring/chat/entities/Message.java
@@ -1,11 +1,12 @@
 package com.substring.chat.entities;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +21,7 @@ public class Message {
     public Message(String sender, String content) {
         this.sender = sender;
         this.content = content;
-        this.timeStamp = LocalDateTime.now();
+        // this.timeStamp = LocalDateTime.now();
+        this.timeStamp = LocalDateTime.now(ZoneOffset.UTC); 
     }
 }
